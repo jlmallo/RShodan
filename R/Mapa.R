fd03be021a2258444196f85452980a0d7bdf6211
dgabcn <dga_bcn1@hotmail.com>
2016-12-17 18:02:18 +0100

    Actualizado

--- R/Mapa.R
+++ R/Mapa.R
@@ -4,12 +4,10 @@
 #' @example
 #' PintarMapa()
 
-PintarMapa <- function() {
-
-  data <- ConsultarShodan()
+PintarMapa <- function(joinedDF) {
 
   #obetener localizaciones webcams:
-  locations <- geolocate(data[[1]][['ip_str']])
+  locations <- geolocate(joinedDF$IP)
 
   #preparar y pintar el mapa:
   world <- map("world", fill = TRUE, plot = FALSE)
